c4830e5b7c273810223910a30729df10ecc00dac
Unaipi <unai.heredero@ikasle.aeg.eus>
2023-04-20 13:43:56 +0200

    [NEW] Verify user funcional con password hasheado y con username

--- routes/verifyUser.php
+++ routes/verifyUser.php
@@ -3,15 +3,15 @@
 require_once (__DIR__.'/../functions.php');
 require_once (__DIR__.'/../controller/Controller.php');
 
+
 if(isset($_POST['name']) && isset($_POST['password']))
 {
         $name   = sanitizeString($_POST['name']);
         $password   = sanitizeString($_POST['password']);
-
         $userSend['name']   = "";
         $userSend['password']   = "";
         $userSend['error']   = "";
-
+        
 
     if($name == "" || $password == "")
     {
@@ -20,16 +20,16 @@ if(isset($_POST['name']) && isset($_POST['password']))
     else
     {
          $resultArray = $user ->getAllByColumn("name",$name);
-
          if($resultArray == null)
          {
              $userSend['error'] ='Name erroneo';
          }
+
          else
          {
-            $hash ='$2y$10$QJ7GcWeP9N7RXWWGepnTb.H9mTNFbhzf4NLD1J6ZSypgdo9V0ZUXa';
-
-             if(password_verify($password, $hash))
+            // echo $password;
+            // echo $resultArray[0]['password'];
+             if(password_verify($password, $resultArray[0]['password']))
              {
                  $userSend = array();
                  foreach ($resultArray as $row)
@@ -37,36 +37,19 @@ if(isset($_POST['name']) && isset($_POST['password']))
                      
                      unset($row['password']);
                      $userSend[] = $row;
-                 }
-             }
-            else
-            {
-            $userSend ['error'] = "Error password";
-            echo password_verify(password_verify($password, $hash));
-            //echo $resultArray[0]['password'];
-
-
-            
-            } 
-         }
+                    }
+                }
+                else
+                {
+                    $userSend ['error'] = "Error password";
+                    
+                    //echo password_verify($password, $hash);
+                    //echo $resultArray[0]['password'];
+                } 
+            }
+            $json_user = json_encode($userSend);
+            echo $json_user;
     }
-//     {
-//         $resultArray = $user->getAllBy2Columns("name", $name, "password", $password);
-        
-//         if($resultArray == null)
-//         {
-//             $userSend['error'] = "Invalid login attempt";
-//         }
-//         else
-//         {
-//             $userSend['name']   = $name;
-//             $userSend['password']   = $password;
-//         }
-
-//     }
-
-    $json_user = json_encode($userSend);
-    echo $json_user;
 }
 else
 {
